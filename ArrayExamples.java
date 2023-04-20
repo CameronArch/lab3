6dccf2ad9d42760d06083f1dfa89f19e0bdec67c
CameronArch <cameronarch598@gmail.com>
2023-04-19 17:34:21 -0700

    Update ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -7,7 +7,7 @@ public class ArrayExamples {
     for(int i = 0; i < arr.length/2; i += 1) {
       int temp = arr[i];
       arr[i] = arr[arr.length - i - 1];
-      arr[i] = temp;
+      arr[arr.length - i-1] = temp;
     }
   }
 
