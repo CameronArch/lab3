e5f72eba452a09f9bc8dbf66330df0d259f7c799
CameronArch <cameronarch598@gmail.com>
2023-04-21 14:03:09 -0700

    lab report 2 changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,9 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+	}
+  @Test 
+	public void testReverseInPlaceFail() {
     int[] input2 = {1,2,3};
     ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3,2,1 }, input2);
@@ -28,3 +31,5 @@ public class ArrayTests {
     
   }
 }
+
+
--- LinkedListTests.java
+++ LinkedListTests.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 
 public class LinkedListTests {
-    @Test
-    public void 
+    
 }
